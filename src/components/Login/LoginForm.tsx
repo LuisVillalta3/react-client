a51bb350b34957c5f67015c8c1bebd4f3e47687b
luis villalta <luisvillalta3@gmail.com>
2020-02-23 18:43:45 -0600

    form

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,31 +1,70 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import { IState } from '../../interfaces/IState';
+import { IProps } from '../../interfaces/IProps';
 
-// import { User } from '../../models/User';
+import { User } from '../../models/User';
 
 import './LoginForm.scss';
 
-class LoginForm extends Component {
+class LoginForm extends Component<IProps,IState> {
 
-  // constructor(props: any) {
-  //   super(props);
-  // }
+  constructor(props: any) {
+    super(props);
 
-  // handleChange(event: any) {
-  //   this.setState({value: event.target.value});
-  // }
+    this.state = {
+      error: false,
+      email: '',
+      password: '',
+      users: []
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInputChange(event: any): void {
+    const target = event.target;
+    const value  = target.value;
+    const name   = target.name;
+    
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit(ev: any): void {
+    ev.preventDefault();
+    const api = 'http://127.0.0.1:5000/api/users';
+    fetch(api)
+      .then(res => res.json)
+      .then(
+        (result) => {
+          this.setState({
+            users: result
+          })
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+    this.state.users.forEach(el => {
+      if (this.state.email === el.email && this.state.password === el.password) {
+        
+      }
+    });
+  }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div className="alert-danger"></div>
         <div className="control">
           <label>Correo electrónico:</label>
-          <input type="email" required />
+          <input name="email" type="email" onChange={this.handleInputChange} value={this.state.email} required />
         </div>
         <div className="control">
           <label>Contraseña:</label>
-          <input type="password" required />
+          <input name="password" type="password" onChange={this.handleInputChange} value={this.state.password} required />
         </div>
         <button className="btn btn-success" type="submit">Ingresar</button>
       </form>
